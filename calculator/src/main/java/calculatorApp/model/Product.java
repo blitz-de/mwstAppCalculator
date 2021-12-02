d0a81288cbd6e37af15e056955a8c775572ed9db
blitz-de <sakhr270@gmail.com>
2021-12-02 13:02:17 +0100

    first commit

---
+++ calculator/src/main/java/calculatorApp/model/Product.java
@@ -0,0 +1,15 @@
+package calculatorApp.model;
+
+public class Product {
+
+    private Integer id;
+    private Long price;
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
