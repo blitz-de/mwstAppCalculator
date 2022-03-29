ded74642a9fe0c1b4b663224b2abbb3270a8cd74
blitz-de <sakhr270@gmail.com>
2022-03-30 01:17:03 +0200

    1. added Dockerfile 2. added logger 3. edited properties.yml

--- api-gateway/src/main/java/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/api_gateway/ApiGatewayApplication.java
@@ -1,23 +1,24 @@
 package api_gateway;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-    @RequestMapping("/")
-    public String home(){
-        return "Eureka Client application";
+    @Bean
+    HttpClient httpClient() {
+        return HttpClient.create().wiretap("LoggingFilter", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
     }
 }
 
